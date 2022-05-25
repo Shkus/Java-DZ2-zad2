c2299bc3260c5b89837df474e98a787513daaebd
Василий Шкурихин <virtualshkus@yandex.ru>
2022-05-25 22:50:20 +0500

    remove folder out

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int account = 100; // количество денег на текущем счету пользователя
-        int addition = 900; // сумма пополнения счета пользователя
+        int addition = 10_000; // сумма пополнения счета пользователя
 
         int bonus = 100; // количество бонусов на текущем счету пользователя
         int costBonus = 100; // стоимость одного бонуса
@@ -14,7 +14,7 @@ public class Main {
         int totalBonus = bonus + calculationBonus; // Итоговое количество бонусных рублей
         int finalScore = account + addition; // Итоговое количество денег на счету пользователя
 
-        if (addition >= 1000) {
+        if (addition > 1000) {
 
             System.out.println("Итоговый счет:" + finalScore + "руб." + " Количество бонусных рублей: " + totalBonus + "руб.");
         } else {
